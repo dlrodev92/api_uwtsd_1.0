c5f9eb21cb081376348c51f01e2a97f9f1c6a751
dlrodev92 <dlrdev92@gmail.com>
2024-10-15 16:25:18 +0100

    cors installed 23

--- uwtsd_api/settings.py
+++ uwtsd_api/settings.py
@@ -154,6 +154,28 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
